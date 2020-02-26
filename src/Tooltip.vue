4caefbde95d4052ebf8cc1c6fefc014a88421fa5
openorclose <interfiancee@gmail.com>
2020-02-26 23:30:00 +0800

    Change tooltip to have underline dotted trigger
    Tooltips and popovers should have the same style triggers.
    
    Let's change tooltips to follow the popover style.

--- src/Tooltip.vue
+++ src/Tooltip.vue
@@ -44,7 +44,8 @@ export default {
   },
   mounted () {
     if (this.$refs.trigger) {
-      this.$refs.trigger.style['border-bottom'] = '1px dashed currentColor'
+      this.$refs.trigger.style['-webkit-text-decoration'] = 'underline dotted';
+      this.$refs.trigger.style['text-decoration'] = 'underline dotted';
     }
   }
 }
